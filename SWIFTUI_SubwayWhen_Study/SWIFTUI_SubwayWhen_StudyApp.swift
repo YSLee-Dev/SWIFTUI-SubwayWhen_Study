c2da99ded3b52ea669cf137508b2f54b85f57641
YoonSuLee <ysleedev01@gmail.com>
2023-04-25 18:24:52 +0900

    Initial Commit

---
+++ SWIFTUI_SubwayWhen_Study/SWIFTUI_SubwayWhen_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTUI_SubwayWhen_StudyApp.swift
+//  SWIFTUI_SubwayWhen_Study
+//
+//  Created by 이윤수 on 2023/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTUI_SubwayWhen_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
