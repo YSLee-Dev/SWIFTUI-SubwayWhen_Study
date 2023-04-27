fc63246d1ed8279adc4f62cda217ca667e7d49e5
YoonSuLee <ysleedev01@gmail.com>
2023-04-27 20:05:12 +0900

    Feat(Main): GroupView Title 추가

--- SWIFTUI_SubwayWhen_Study/Main/GroupView/GroupView.swift
+++ SWIFTUI_SubwayWhen_Study/Main/GroupView/GroupView.swift
@@ -14,52 +14,59 @@ struct GroupView: View {
     
     var body: some View {
         GeometryReader{ geomotry in
-            HStack(alignment: .center, spacing: 5){
-                ForEach(groupList, id: \.self){ data in
-                    
-                    if data == groupSelete{
-                        Button {
-                            print(data)
-                            if data != groupSelete {
-                                groupSelete = groupSelete == "출근" ? "퇴근" : "출근"
-                            }
-                        } label: {
-                            groupViewButton(title: data, isSeleted: true)
-                        }
-                        .frame(width: geomotry.size.width - 80,
-                               height: 40,
-                               alignment: .center
-                        )
-                        .overlay{
-                            RoundedRectangle(cornerRadius: 15)
-                                .strokeBorder()
-                                .padding(1.5)
-                        }
+            VStack(alignment: .leading){
+                Text("실시간 현황")
+                    .foregroundColor(Color.gray)
+                    .fontWeight(.semibold)
+                    .font(.system(size: 15))
+                
+                HStack(alignment: .center, spacing: 5){
+                    ForEach(groupList, id: \.self){ data in
                         
-                        
-                    }else{
-                        Button {
-                            print(data)
-                            if data != groupSelete {
-                                groupSelete = groupSelete == "출근" ? "퇴근" : "출근"
+                        if data == groupSelete{
+                            Button {
+                                print(data)
+                                if data != groupSelete {
+                                    groupSelete = groupSelete == "출근" ? "퇴근" : "출근"
+                                }
+                            } label: {
+                                groupViewButton(title: data, isSeleted: true)
                             }
-                        } label: {
-                            groupViewButton(title: data, isSeleted: false)
-                        }
-                        .frame(width: geomotry.size.width - (geomotry.size.width - 75),
+                            .frame(width: geomotry.size.width - 80,
+                                   height: 40,
+                                   alignment: .center
+                            )
+                            .overlay{
+                                RoundedRectangle(cornerRadius: 15)
+                                    .strokeBorder()
+                                    .padding(1.5)
+                            }
+                            
+                            
+                        }else{
+                            Button {
+                                print(data)
+                                if data != groupSelete {
+                                    groupSelete = groupSelete == "출근" ? "퇴근" : "출근"
+                                }
+                            } label: {
+                                groupViewButton(title: data, isSeleted: false)
+                            }
+                            .frame(width: geomotry.size.width - (geomotry.size.width - 75),
                                    height: 40,
                                    alignment: .center
                             )
                             .cornerRadius(20)
+                        }
                     }
                 }
+                .background(.gray.opacity(0.1))
+                .cornerRadius(20)
+                
+                Spacer()
             }
-            Spacer()
-            
         }
-        .background(.gray.opacity(0.1))
-        .cornerRadius(20)
-        .frame(height: 40)
+        .frame(height: 65)
         
     }
 }
